4fcb9977d2d1e1809798a9d7e619043f7e567d57
mounir92 <mounir92600@hotmail.fr>
2016-01-10 02:24:54 +0100

    new get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmouhssi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 11:04:32 by mmouhssi          #+#    #+#             */
-/*   Updated: 2015/12/21 17:31:01 by mmouhssi         ###   ########.fr       */
+/*   Updated: 2016/01/10 02:10:41 by mmouhssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,153 +17,67 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
-void		ft_add_lst(int const fd, t_gnl *list, char *buf)
-{
-	int		i;
-	t_gnl	*lst;
-
-	i = 0;
-	lst = list; //
-	lst->fd = fd;
-	lst->s = (char *)malloc(ft_strlen(buf + 1));
-	lst->next = NULL;
-	while (buf[i] != '\0')
-	{
-		lst->s[i] = buf[i];
-		i++;
-	}
-}
-
-char		*ft_strjoin_gnl(char *s1, char *s2)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0' && s2[j] != '\n')
-	{
-		str[i] = s2[j];
-		j++;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-void		ft_del_gnl(char *s)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	str = (char *)malloc(ft_strlen(s) + 1);
-	ft_bzero(str, ft_strlen(s) + 1);
-	while (s[i] != '\n' && s[i] != '\0')
-		i++;
-	if (s[i] == '\n')
-		i++;
-	while (s[i] != '\0')
-	{
-		str[j] = s[i];
-		j++;
-		i++;
-	}
-	str[j] = '\0';
-	if (s[i] == '\0')
-		ft_bzero(s, ft_strlen(s));
-	else
-		s = str;
-	free(str);
-}
-
-void		ft_read_gnl(int const fd, t_gnl *lst, char **str)
-{
-	char	*buff;
-	int		i;
-
-	buff = (char *)malloc(BUFF_SIZE + 1);
-	ft_bzero(buff, BUFF_SIZE + 1);
-	while (read(fd, buff, BUFF_SIZE) != 0)
-	{
-		i = 0;
-		*str = ft_strjoin_gnl(*str, buff);
-		while (buff[i] != '\0')
-		{
-			if (buff[i] == EOF || buff[i] == '\n')
-				break ;
-			i++;
-		}
-		if (buff[i] == EOF || buff[i] == '\n')
-			break ;
-		ft_bzero(buff, BUFF_SIZE);
-	}
-	if (buff[i] == '\n')
-		i++;
-	if (buff[i] != EOF && buff[i] != '\0')
-		ft_add_lst(fd, lst, &buff[i]);
-	free(buff);
-}
-
 int			get_next_line(int const fd, char **line)
 {
-	static t_gnl	*lst;
-	char			*str;
+	char			buff[BUFF_SIZE + 1];
+	static char		*str;
+	int			i;
 
-	if (!lst)
-		lst = (t_gnl *)malloc(sizeof(t_gnl));
-	str = (char *)malloc(BUFF_SIZE + 1);
-	ft_bzero(str, BUFF_SIZE + 1);
-	if (fd == lst->fd)
+	if (!str)
 	{
-		str = ft_strcat(str, lst->s);
-		ft_del_gnl(lst->s);
+		str = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
+		ft_bzero(str, BUFF_SIZE + 1);
 	}
-	ft_read_gnl(fd, lst, &str);
-	*line = (char *)malloc(ft_strlen(str) + 1);
-	ft_bzero(*line, ft_strlen(str) + 1);
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	ft_bzero(buff, BUFF_SIZE + 1);
+	if (str[i] != '\n')
+	{
+		while (read(fd, buff, BUFF_SIZE) != 0)
+		{
+			i = 0;
+			str = ft_strjoin(str, buff);
+			while (str[i] != '\n' && str[i] != '\0')
+				i++;
+			if (str[i] == '\n')
+				break ;
+			ft_bzero(buff, BUFF_SIZE);
+		}
+	}
+	*line = (char *)malloc(i + 1);
+	ft_bzero(*line, i + 1);
+	ft_strncat(*line, str, i);
 	if (str[0] != '\0')
-		ft_strcat(*line, str);
-	free(str);
+		str = ft_strsub(str, i + 1, ft_strlen(str) - i);
 	return (0);
 }
-
+/*
 int			main()
 {
 	int f;
 	char **tab;
-	char buf[BUFF_SIZE];
+	//char buf[BUFF_SIZE];
 	int i;
 	
 	i = 0;
-	tab =(char **)malloc(sizeof(char *) * 7);
-	while (i < 7)
+	tab =(char **)malloc(sizeof(char *) * 147);
+	f = open("get_next_line_V2.c", O_RDONLY);
+	while (i < 147)
 	{
 		tab[i] = (char *)malloc(100);
 		ft_bzero(tab[i], 100);
+		get_next_line(f, &tab[i]);
 		i++;
 	}
-	tab[7] = NULL;
-	ft_bzero(buf, BUFF_SIZE);
-	f = open("test", O_RDONLY);
-	/*read(f, buf, BUFF_SIZE - 1);
-	ft_putstr(buf);*/
-	get_next_line(f, tab);
+	tab[147] = NULL;
+	//ft_bzero(buf, BUFF_SIZE);
+	//read(f, buf, BUFF_SIZE - 1);
+	//get_next_line(f, tab);
+	//get_next_line(f, &(tab[1]));
 	//ft_putendl_tab(tab);
-	get_next_line(f, &(tab[1]));
-	get_next_line(f, &(tab[2]));
-	get_next_line(f, &(tab[3]));
-	get_next_line(f, &(tab[4]));
-	get_next_line(f, &(tab[5]));
-	get_next_line(f, &(tab[6]));
+	//ft_putstr(tab[0]);
+	//ft_putchar('\n');
+	//ft_putstr(tab[1]);
 	ft_putendl_tab(tab);
-}
+}*/
